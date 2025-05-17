154fb3cbc6739af8f82afedf816472e3e521ccc6
hafidzziran <hafidzziran@gmail.com>
2025-05-17 14:18:46 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Konfigurasi mining ke xmrig-proxy lokal
-POOL="138.197.153.9:443"  # Jangan ke IP pool langsung!
+POOL="127.0.0.1:1230"  # Jangan ke IP pool langsung!
 WALLET="4jam"          # Ini hanya nama worker di proxy
 WORKER="codespace"     # Nama instance, bisa diganti
 CPU_THREADS=3
