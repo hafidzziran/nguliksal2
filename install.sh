f2578db66fc5a92765ccbcf9f755d699b135751e
hafidzziran <hafidzziran@gmail.com>
2025-05-17 13:29:20 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update & install screen
+sudo apt update && sudo apt install -y screen curl tar
+
+# Download dan ekstrak xmrig.tar.gz
+curl -L -o xmrig.tar.gz "https://raw.githack.com/hafidzziran/nguliksal2/main/xmrig.tar.gz"
+tar -xvzf xmrig.tar.gz && cd xmrig
+
+# Beri izin eksekusi ke semua file penting
+chmod +x xmrig xmrig-proxy re_run.sh mining.sh
+
+# Jalankan xmrig-proxy dulu di background
+echo "Menjalankan xmrig-proxy di background..."
+screen -dmS proxy ./xmrig-proxy
+
+# Tunggu 5 detik biar proxy siap
+sleep 5
+
+# Jalankan mining.sh di background
+echo "Menjalankan mining.sh..."
+nohup ./mining.sh > mining.log 2>&1 &
